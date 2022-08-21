6e38e594e1b210b34c9bc23df9ffe9f1cd20c389
dmitrijgirskij <d.girsky@gmail.com>
2022-08-21 21:35:46 +0300

    export app

--- lib/app.dart
+++ lib/app.dart
@@ -2,20 +2,15 @@ import 'package:ecommerce/routes.dart';
 import 'package:ecommerce/ui/screens/wrapper.dart';
 import 'package:ecommerce/ui/themes/light.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await dotenv.load(fileName: '.env');
-  runApp(const MyApp());
-}
+import 'package:flutter/services.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
     return MaterialApp(
       title: 'Ecommerce concept',
       theme: themeLight,
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,12 @@
+import 'package:ecommerce/ui/screens/cart.dart';
+import 'package:ecommerce/ui/screens/favorite.dart';
+import 'package:ecommerce/ui/screens/profile.dart';
 import 'package:ecommerce/ui/screens/wrapper.dart';
 import 'package:flutter/material.dart';
 
 var routes = <String, WidgetBuilder>{
   '/': (context) => const WrapperScreen(),
+  '/favorite': (context) => const FavoriteScreen(),
+  '/cart': (context) => const CartScreen(),
+  '/profile': (context) => const ProfileScreen(),
 };
