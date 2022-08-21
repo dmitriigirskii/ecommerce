fd2b65a0f416d52bcf85ea2a3aeb8f3929cd19d4
dmitrijgirskij <d.girsky@gmail.com>
2022-08-21 09:48:20 +0300

    create empty product details screen

---
+++ lib/ui/screens/product_details.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProductDetails extends StatelessWidget {
+  const ProductDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Details'),
+      ),
+      body: Center(
+        child: Text('Product Details'),
+      ),
+    );
+  }
+}
