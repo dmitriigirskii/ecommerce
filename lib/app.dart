5c2fbacc8e5bd4038ef125474626b72bebfd8b6a
dmitrijgirskij <d.girsky@gmail.com>
2022-08-21 09:39:14 +0300

    environment

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,12 @@
 import 'package:ecommerce/ui/screens/main.dart';
 import 'package:ecommerce/ui/themes/light.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
