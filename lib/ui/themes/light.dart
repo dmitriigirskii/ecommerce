bc4ae1a3fbe03370513ed197ab310847022880d4
dmitrijgirskij <d.girsky@gmail.com>
2022-08-21 16:14:40 +0300

    update theme

--- lib/ui/themes/light.dart
+++ lib/ui/themes/light.dart
@@ -1,6 +1,8 @@
+import 'package:ecommerce/ui/styles/colors.dart';
 import 'package:flutter/material.dart';
 
 final themeLight = ThemeData(
   fontFamily: 'Mark Pro',
   primaryColor: Colors.black,
+  scaffoldBackgroundColor: cLight1,
 );
